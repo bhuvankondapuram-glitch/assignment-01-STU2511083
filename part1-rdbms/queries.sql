66a5090a2e89a52ef2931d530bfbadbb6ec8dd10
bhuvankondapuram-glitch <bhuvan.kondapuram@gmail.com>
2026-03-21 13:40:39 +0530

    Update queries.sql

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,1 +1,32 @@
+SELECT c.customer_name, SUM(p.unit_price * oi.quantity) AS total_value
+FROM Customers c
+JOIN Orders o ON c.customer_id = o.customer_id
+JOIN Order_Items oi ON o.order_id = oi.order_id
+JOIN Products p ON oi.product_id = p.product_id
+WHERE c.customer_city = 'Mumbai'
+GROUP BY c.customer_name;
 
+SELECT p.product_name, SUM(oi.quantity) AS total_quantity
+FROM Products p
+JOIN Order_Items oi ON p.product_id = oi.product_id
+GROUP BY p.product_name
+ORDER BY total_quantity DESC
+LIMIT 3;
+
+SELECT s.sales_rep_name, COUNT(DISTINCT o.customer_id) AS total_customers
+FROM Sales_Reps s
+LEFT JOIN Orders o ON s.sales_rep_id = o.sales_rep_id
+GROUP BY s.sales_rep_name;
+
+SELECT o.order_id, SUM(p.unit_price * oi.quantity) AS total_value
+FROM Orders o
+JOIN Order_Items oi ON o.order_id = oi.order_id
+JOIN Products p ON oi.product_id = p.product_id
+GROUP BY o.order_id
+HAVING total_value > 10000
+ORDER BY total_value DESC;
+
+SELECT p.product_name
+FROM Products p
+LEFT JOIN Order_Items oi ON p.product_id = oi.product_id
+WHERE oi.product_id IS NULL;
